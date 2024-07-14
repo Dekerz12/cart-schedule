5b6504f267f88f0abca885eb1661e9ed333fdba0
Dan Sedrick Reyes <sedrickreyes.dev@gmail.com>
2024-07-15 07:12:01 +0800

    fix subject

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
@@ -17,10 +16,17 @@ const nextWeek = `${dayjs().weekday(7).day(1).format("MMMM D")} - ${dayjs()
   .weekday(7)
   .format("MMMM D")}`;
 
-export const metadata: Metadata = {
-  title: dayjs().format("dddd") !== "Sunday" ? thisWeek : nextWeek,
-  description: "Local Cart Witnessing of Sta. Rita Congregation",
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
+
+export async function generateMetadata({ params, searchParams }: Props) {
+  return {
+    title: dayjs().format("dddd") !== "Sunday" ? thisWeek : nextWeek,
+    description: "Local Cart Witnessing of Sta. Rita Congregation",
+  };
+}
 
 export default function RootLayout({
   children,
